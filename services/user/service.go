d971ec59bb834ebf00edcdaed1f65b0d7403e8f9
shichengyuan <shichengyuan@gmail.com>
2019-04-06 09:28:39 +0800

    merge

--- services/user/service.go
+++ services/user/service.go
@@ -1,14 +1,12 @@
 package user
 
 import (
-	"hypermedlab/backend-myblog/pkgs/middlewares"
 	mUser "hypermedlab/backend-myblog/models/user"
 	userDB "hypermedlab/backend-myblog/models/user/db"
 	"hypermedlab/backend-myblog/pkgs/forms"
 	"hypermedlab/backend-myblog/pkgs/jwt"
 	"hypermedlab/backend-myblog/pkgs/password"
 	"hypermedlab/backend-myblog/pkgs/uuid"
-	
 
 	"errors"
 	"log"
@@ -28,11 +26,7 @@ func NewService(conn *sqlx.DB) Service {
 }
 
 func (u *user) RegisterUser(form *forms.CreateUser) (*mUser.User, error) {
-<<<<<<< HEAD
-	hashedpwd, err := password.HashPassword(form.Password)
-=======
 	hashedpwd, err := password.HashedPassword(form.Password)
->>>>>>> aad3b9efc1cbeaa934d16aaadcbbefcfce3bd182
 	if err != nil {
 		return nil, err
 	}
@@ -65,7 +59,6 @@ func (u *user) Login(form *forms.LoginForm, secret string) (*mUser.User, error) 
 	}
 
 	usr.Token = token
-	middlewares.
 
 	return usr, nil
 }
@@ -83,11 +76,7 @@ func (u *user) UpdatePassword(form *forms.UpdatePassword) error {
 		return errors.New("password not correct")
 	}
 
-<<<<<<< HEAD
-	hashedPw, err := password.HashPassword(form.Password)
-=======
 	hashedPw, err := password.HashedPassword(form.Password)
->>>>>>> aad3b9efc1cbeaa934d16aaadcbbefcfce3bd182
 	if err != nil {
 		return err
 	}
